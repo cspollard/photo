6fdf24d96ccfd20fa4250ea39006146934493fe2
Chris Pollard <cspollard@gmail.com>
2014-12-31 17:16:16 -0500

    working up to snuff.

--- photosort.hs
+++ photosort.hs
@@ -4,22 +4,21 @@ module Main where
 
 import System.Environment (getArgs)
 import qualified Data.ByteString.Char8 as BSC
-import Data.Attoparsec.ByteString.Lazy hiding (take, map, drop)
+import Data.Attoparsec.ByteString.Lazy (Parser, word8, maybeResult, parse, anyWord8)
 import qualified Data.Attoparsec.ByteString.Lazy as A
 import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString as BS
 
 import Control.Applicative
-import Data.Maybe (catMaybes)
+import Data.Maybe (fromJust, isJust)
 
-import Data.Time.Calendar (fromGregorian)
-import Data.Time.LocalTime (LocalTime(..), TimeOfDay(..))
+import Data.Time (fromGregorian, LocalTime(..), TimeOfDay(..), formatTime, defaultTimeLocale)
 
 import Data.Word
+import System.Directory (renameFile, doesFileExist, createDirectoryIfMissing)
 
 
 digits :: Int -> Parser Int
-digits n = fmap (read . BSC.unpack) $ A.take n
+digits n = (read . BSC.unpack) <$> A.take n
 
 
 char :: Char -> Parser Word8
@@ -46,11 +45,36 @@ findTimestamp = fmap (maybeResult . parse ts) . BSL.readFile
     where ts = timestamp <|> (anyWord8 *> ts)
 
 
+tryMove :: FilePath -> FilePath -> Int -> String -> IO ()
+tryMove fin fout n suffix = do
+    let outfile = fout ++ "." ++ show n ++ suffix
+    exists <- doesFileExist outfile
+    if exists
+        then do
+            print $ "file " ++ outfile ++ " already exists; incrementing suffix."
+            tryMove fin fout (n+1) suffix
+        else renameFile fin outfile
+
+
+moveImage :: String -> String -> IO ()
+moveImage fname dir = do
+    t <- findTimestamp fname
+    let suff = '.' : (reverse . takeWhile (/= '.') . reverse $ fname)
+
+    case t of
+        Nothing -> do
+            print $ "error: " ++ fname ++ " does not have a time stamp; continuing."
+            return ()
+
+        Just ts -> do
+            let outfolder = ((++) dir . formatTime defaultTimeLocale "/%0Y/%m") ts
+            createDirectoryIfMissing True outfolder
+            tryMove fname (outfolder ++ formatTime defaultTimeLocale "/%d-%H.%M.%S" ts) 0 suff
+
+
 
 main :: IO ()
 main = do
     outfolder:infiles <- getArgs
 
-    outfilenames' <- sequence $ map findTimestamp infiles :: IO [Maybe LocalTime]
-
-    print outfilenames'
+    mapM_ (flip moveImage outfolder) infiles
